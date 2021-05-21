ab316163bc3d2b1bb7e9626fdad7ed4a144c4d48
AlexDev-13 <alexander.asanbekov@gmail.com>
2021-05-21 13:11:38 +0600

    добавил swagger и возможность загружать файлы вместе с заявкой

--- src/main/java/com/gov/erf/controller/claim/ClaimController.java
+++ src/main/java/com/gov/erf/controller/claim/ClaimController.java
@@ -6,7 +6,10 @@ import com.gov.erf.dto.http.claims.request.AddClaimRequestDto;
 import com.gov.erf.endpoint.claim.ClaimEndpoint;
 import io.swagger.annotations.ApiParam;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
@@ -23,11 +26,11 @@ public class ClaimController {
     }
 
     @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    ClaimDto create(
+    public ClaimDto create(
             @RequestPart("request") @ApiParam("AddClaimRequestDto") AddClaimRequestDto addClaimRequestDto,
             @RequestPart(value = "file", required = false) MultipartFile file
     ) throws Exception {
-       return claimEndpoint.create(addClaimRequestDto,file);
+        return claimEndpoint.create(addClaimRequestDto, file);
     }
 
 }
