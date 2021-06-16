fcc8076035ca3bfe7d9327ecc10f5a5541b835b1
alexdev-13 <feel.invincible.13@gmail.com>
2021-06-17 01:34:55 +0600

    изменен Claim

--- src/main/java/com/gov/erf/dto/http/tables/request/InfoRequestFromResponsibleBodyDto.java
+++ src/main/java/com/gov/erf/dto/http/tables/request/InfoRequestFromResponsibleBodyDto.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 @Builder
 public class InfoRequestFromResponsibleBodyDto {
+
     private Long claimId;
     private String cause;
     private Boolean decision;
---
+++ src/main/resources/db/migration/V1/V1.1623871764__add_default_admin.sql
@@ -0,0 +1,5 @@
+INSERT INTO admin (name, surname, patronymic, username, email, password, role_id, locked, enabled)
+VALUES ('admin', '', '', 'admin', '', 'admin', 1, false, true);
+
+INSERT INTO admin (name, surname, patronymic, username, email, password, role_id, locked, enabled)
+VALUES ('admin2', '', '', 'admin2', '', 'admin', 2, false, true);
