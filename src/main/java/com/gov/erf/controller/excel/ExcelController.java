2e8367cba33827c3d43b55556f092bc42cc5ae79
alexdev-13 <feel.invincible.13@gmail.com>
2021-06-20 23:59:12 +0600

    добавлен excel export

---
+++ src/main/java/com/gov/erf/controller/excel/ExcelController.java
@@ -0,0 +1,40 @@
+package com.gov.erf.controller.excel;
+
+import com.gov.erf.models.claims.Claim;
+import com.gov.erf.repository.claim.ClaimRepository;
+import com.gov.erf.service.excel.ExcelServiceExport;
+import com.gov.erf.service.excel.impl.DefaultExcelServiceExport;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collection;
+
+@RestController
+@RequestMapping("/api/v1/excel")
+public class ExcelController {
+
+    private final ClaimRepository claimRepository;
+
+
+    public ExcelController(ClaimRepository claimRepository) {
+        this.claimRepository = claimRepository;
+    }
+
+    @GetMapping
+    public void exportToExcel(HttpServletResponse response) throws IOException {
+        response.setContentType("application/octet-stream");
+        String headerKey = "Content Disposition";
+        String headerValue = "attachment; filename=ClaimExport.xlsx";
+
+        response.setHeader(headerKey, headerValue);
+
+        Collection<Claim> claims = claimRepository.findAll();
+
+        ExcelServiceExport excelServiceExport = new DefaultExcelServiceExport(claims);
+        excelServiceExport.export(response);
+    }
+
+}
