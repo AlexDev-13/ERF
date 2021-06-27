80ce3d22b71da806bd9c94f5db440c104d53487a
alexdev-13 <feel.invincible.13@gmail.com>
2021-06-27 14:42:29 +0600

    фильтрация заявок

--- src/main/java/com/gov/erf/config/security/WebSecurityConfig.java
+++ src/main/java/com/gov/erf/config/security/WebSecurityConfig.java
@@ -27,14 +27,14 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter implements W
     private final AccountService accountService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
-                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
-                        "Access-Control-Request-Headers")
-                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
-                .allowCredentials(true).maxAge(3600);
-    }
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/*").allowedOrigins("*").allowedMethods("GET", "POST", "OPTIONS", "PUT")
+//                .allowedHeaders("Content-Type", "X-Requested-With", "accept", "Origin", "Access-Control-Request-Method",
+//                        "Access-Control-Request-Headers")
+//                .exposedHeaders("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials")
+//                .allowCredentials(true).maxAge(3600);
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
