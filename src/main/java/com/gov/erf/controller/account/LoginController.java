5b4571b6ee0a5a9db4b7cdb3fb8877f2caa5c92b
alexdev-13 <feel.invincible.13@gmail.com>
2021-07-04 16:53:34 +0600

    новая фильтрация

--- src/main/java/com/gov/erf/controller/account/LoginController.java
+++ src/main/java/com/gov/erf/controller/account/LoginController.java
@@ -4,13 +4,13 @@ import com.gov.erf.dto.http.account.UserDto;
 import com.gov.erf.mapper.account.AccountMapper;
 import com.gov.erf.models.account.Admin;
 import com.gov.erf.service.account.AccountService;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/login")
+@RequestMapping("/api/v1/login")
 public class LoginController {
 
     private final AccountService accountService;
@@ -22,9 +22,12 @@ public class LoginController {
     }
 
     @GetMapping
-    public UserDto getUser(@RequestParam("username") String username, @RequestParam("password") String password) {
-        Admin admin = (Admin) accountService.loadUserByUsername(username);
-        return accountMapper.toUserDto(admin);
+    public UserDto getUser(@AuthenticationPrincipal Admin admin) {
+
+        String username = admin.getUsername();
+
+        Admin admin1 = (Admin) accountService.loadUserByUsername(username);
+        return accountMapper.toUserDto(admin1);
     }
 
 }
